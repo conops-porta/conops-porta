55e263cad397edda6895a6e635fa782d6abaeb8a
Edith Emmings <emmings.edith@gmail.com>
2019-11-14 10:17:55 -0600

    date formatted for table heading

--- src/components/VolunteerSchedule/Manage.jsx
+++ src/components/VolunteerSchedule/Manage.jsx
@@ -10,57 +10,93 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import axios from 'axios'
+import moment from 'moment'
+
+
+const findLongestListOfShifts = (arrayOfRoles) => {
+    let longestList = 0
+    let arrayOfTimesToReturn = [];
+    arrayOfRoles.forEach(role => {
+        let allShiftTimes = [];
+        // pushes all shifts date/time into array
+        role.shifts.forEach(shift => { allShiftTimes.push(`${shift.ShiftDate}|${shift.ShiftTime}`) })
+        // filters out duplicates
+        let unique = allShiftTimes.filter((item, i, ar) => ar.indexOf(item) === i);
+        // if the unique shifts are longest, we'll use their date/times as a 
+        if (unique.length > longestList) {
+            longestList = unique.length;
+            unique.forEach(shift => {
+                var arr = shift.split('|'); // breaks apart time and date to be used in an object instead of array
+                let time = arr[1].slice(0, -3) // removes "seconds" section from the time formatting
+                arrayOfTimesToReturn.push({ ShiftDate: arr[0], ShiftTime: time })
+            })
+        }
+    })
+    console.log(arrayOfTimesToReturn)
+    return arrayOfTimesToReturn
+}
+
 
 class Manage extends Component {
-
+    state = {}
     componentDidMount() {
         axios.get('/api/volunteer/shifts')
-        .then(response => {
-            console.log(response.data)
-            this.setState({
-                data: response.data
+            .then(response => {
+                console.log(response.data)
+                this.setState({
+                    ...this.state,
+                    data: response.data,
+                    shiftTimes: findLongestListOfShifts(response.data)
+                })
+            }).catch(error => {
+                console.log(error)
             })
-        }).catch(error => {
-            console.log(error)
-        })
     }
+
+
 
     render() {
         return (
             <div className="Manage">
                 <h1>MANAGE</h1>
-
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <TableRow >
-                            <ExpansionPanel>
-                                <ExpansionPanelSummary
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls="panel1a-content"
-                                    id="panel1a-header"
-                                >
-                                    <Typography >Expansion Panel 1</Typography>
-                                </ExpansionPanelSummary>
-                                <ExpansionPanelDetails>
-                                    <Typography>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                                        sit amet blandit leo lobortis eget.
+                {this.state.data ?
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Department</TableCell>
+                                <TableCell>Role</TableCell>
+                                <TableCell>Walk-up approved?</TableCell>
+                                {this.state.shiftTimes.map(shift => (
+                                    <TableCell>
+                                        {moment(shift.ShiftDate).format('dddd')}, {shift.ShiftTime}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow >
+                                <ExpansionPanel>
+                                    <ExpansionPanelSummary
+                                        expandIcon={<ExpandMoreIcon />}
+                                        aria-controls="panel1a-content"
+                                        id="panel1a-header"
+                                    >
+                                        <Typography >Expansion Panel 1</Typography>
+                                    </ExpansionPanelSummary>
+                                    <ExpansionPanelDetails>
+                                        <Typography>
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                            sit amet blandit leo lobortis eget.
                                     </Typography>
 
-                                    <TableCell>
-                                    </TableCell>
-                                </ExpansionPanelDetails>
-                            </ExpansionPanel>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-
+                                        <TableCell>
+                                        </TableCell>
+                                    </ExpansionPanelDetails>
+                                </ExpansionPanel>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                    : ''}
 
 
             </div >
