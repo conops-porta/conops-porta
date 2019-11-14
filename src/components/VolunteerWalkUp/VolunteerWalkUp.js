2cc60bee3a43bac15553a4755138a2574bf16fe9
David Diaz <daemdisa@aol.com>
2019-11-14 11:50:03 -0600

    showing exists false and true with the json.stringify

--- src/components/VolunteerWalkUp/VolunteerWalkUp.js
+++ src/components/VolunteerWalkUp/VolunteerWalkUp.js
@@ -10,18 +10,12 @@ class VolunteerWalkUp extends Component {
     console.log('btn click', this.props.reduxStore.VolunteerWalkUpReducer.VolunteerWalkUpReducer)
     event.preventDefault();
 
-    if(this.props.reduxStore.VolunteerWalkUpReducer.VolunteerWalkUpReducer){
       this.props.dispatch({
         type: 'FETCH_WALKUP_SHIFTS',
         payload: {
           badgeNumber: this.state.badgeNumber
         }
       });
-      alert ('correct')
-    } else {
-      alert ('wrong')
-      this.props.dispatch({type: 'BADGE_ERROR'});
-    }
   } // end registeredUsers
 
   handleInputChange = propertyName => (event) => {
@@ -33,25 +27,20 @@ class VolunteerWalkUp extends Component {
   render() {
     return (
       <div>
-        {/* <h1>Hats are cool</h1>
-        {this.props.reduxStore.VolunteerWalkUpReducer.VolunteerWalkUpReducer && (
-          <h2
-            className="alert"
-            role="alert"
-          >
-            {this.props.reduxStore.VolunteerWalkUpReducer.VolunteerWalkUpReducer}
-          </h2>
-        )} */}
         {JSON.stringify(this.props.reduxStore.VolunteerWalkUpReducer.VolunteerWalkUpReducer)}
-        {JSON.stringify(this.state)}
+        {/* {JSON.stringify(this.state)} */}
         <h1>Walk-Up Volunteer</h1>
           <h2>Sign In</h2>
             <form onSubmit={this.walkUpBadgeNumberSubmit}>
               <p>Badge #
               <br/>
-                <input value={this.state.badgeNumber} type="number" placeholder="badge number" value={this.state.badgeNumber} onChange={this.handleInputChange('badgeNumber')}></input>
-                {/* {this.validator.message('badgeNumber', this.state.badgeNumber, 'required|numeric')} */}
-
+                <input 
+                  value={this.state.badgeNumber} 
+                  type="number" 
+                  placeholder="badge number" 
+                  value={this.state.badgeNumber} 
+                  onChange={this.handleInputChange('badgeNumber')}>
+                </input>
                 <button>Go</button>
               </p>
             </form>
