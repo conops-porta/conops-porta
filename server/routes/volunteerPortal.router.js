3858584232b84a1524e99745427c89c0c2de0781
Lilith Bentley <sbentley@gmail.com>
2019-11-15 13:05:36 -0600

    Attempt volunteer hours get; NOT TESTED; NOT FINAL

--- server/routes/volunteerPortal.router.js
+++ server/routes/volunteerPortal.router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const { rejectNonVetted } = require('../modules/isVettedVolunteerAuthentication-middleware');
 const pool = require('../modules/pool');
-const moment = require('moment');
 const router = express.Router();
 
 
@@ -20,7 +19,39 @@ router.get('/', rejectUnauthenticated, rejectNonVetted, (req, res) => {
 })
 
 router.get('/hours', rejectUnauthenticated, rejectNonVetted, (req, res) => {
-    let queryText = ``;
+    let queryText = `
+        SELECT "Attendee"."BadgeNumber", "Attendee"."FirstName", "Attendee"."LastName", "VolunteerContact"."VolunteerDiscord",
+            (
+                "VolunteerContact"."VolunteerHours"
+                + (
+                    SELECT COUNT(*) FROM "Shift"
+                    JOIN "Attendee" ON "Shift"."BadgeNumber" = "Attendee"."BadgeNumber"
+                    WHERE "Shift"."BadgeNumber" = "Attendee"."BadgeNumber"
+                    AND "Shift"."NoShow" = false
+                    AND "Shift"."ShiftDate" < CURRENT_DATE
+                    GROUP BY "Shift"."BadgeNumber"
+                )
+                + (
+                    SELECT COUNT(*) FROM "Shift"
+                    JOIN "Attendee" ON "Shift"."BadgeNumber" = "Attendee"."BadgeNumber"
+                    WHERE "Shift"."BadgeNumber" = "Attendee"."BadgeNumber"
+                    AND "Shift"."NoShow" = false
+                    AND "Shift"."ShiftDate" = CURRENT_DATE
+                    AND "Shift"."ShiftTime" < CURRENT_TIME
+                    GROUP BY "Shift"."BadgeNumber"
+                )
+            ) AS "HoursWorked",
+            (
+                SELECT COUNT(*) FROM "Shift"
+                JOIN "Attendee" ON "Shift"."BadgeNumber" = "Attendee"."BadgeNumber"
+                WHERE "Shift"."BadgeNumber" = "Attendee"."BadgeNumber"
+                GROUP BY "Shift"."BadgeNumber"
+            ) AS "HoursScheduled"
+        FROM "Shift"
+        JOIN "Attendee" ON "Shift"."BadgeNumber" = "Attendee"."BadgeNumber"
+        JOIN "VolunteerContact" ON "Attendee"."VolunteerID" = "VolunteerContact"."VolunteerID"
+        GROUP BY "Shift"."BadgeNumber";
+    `;
     pool.query(queryText)
         .then((result) => {
             console.log('in volunteer hours GET router:', result.rows);
