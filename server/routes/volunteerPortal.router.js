74dbe6ed3497c786ae5ca9d72c2a686b8241cd90
Lilith Bentley <sbentley@gmail.com>
2019-11-15 10:19:29 -0600

    Add GET /volunteer-portal/hours/ route

--- server/routes/volunteerPortal.router.js
+++ server/routes/volunteerPortal.router.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const { rejectNonVetted } = require('../modules/isVettedVolunteerAuthentication-middleware');
 const pool = require('../modules/pool');
+const moment = require('moment');
 const router = express.Router();
 
 
-router.get('/', rejectUnauthenticated, (req, res) => {
-    let queryText = ``
+router.get('/', rejectUnauthenticated, rejectNonVetted, (req, res) => {
+    let queryText = ``;
     pool.query(queryText)
         .then((result) => {
             console.log('in volunteer GET router:', result.rows);
@@ -18,6 +19,18 @@ router.get('/', rejectUnauthenticated, (req, res) => {
         })
 })
 
+router.get('/hours', rejectUnauthenticated, rejectNonVetted, (req, res) => {
+    let queryText = ``;
+    pool.query(queryText)
+        .then((result) => {
+            console.log('in volunteer hours GET router:', result.rows);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log('error in volunteer hours GET router:', error)
+            res.sendStatus(500)
+        })
+})
 
 
 module.exports = router;
