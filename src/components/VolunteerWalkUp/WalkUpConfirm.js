0d692ff804e5d4b7d700b328f24e30b5d5d619f0
David Diaz <daemdisa@aol.com>
2019-11-15 19:35:47 -0600

    checking the confirm

--- src/components/VolunteerWalkUp/WalkUpConfirm.js
+++ src/components/VolunteerWalkUp/WalkUpConfirm.js
@@ -4,87 +4,32 @@ import moment from 'moment';
 import axios from 'axios';
 import swal from 'sweetalert';
 
+let info;
+
 class WalkUpConfirm extends Component {
 
-    state = {
-        volunteerFirstName: '',
-        discordName: '',
-        phoneNumber: ''
-      }
+    // state = {
+    //     volunteerFirstName: '',
+    //     discordName: '',
+    //     phoneNumber: ''
+    //   }
 
-      handleInputChange = propertyName => (event) => {
-        this.setState({
-          [propertyName]: event.target.value,
-        });
-      } // end handleInputChange
-
-      verifyInfo = () => {
-        console.log('confirm btn click')
-        
-        // swal({
-        //   title: `Thank You`,
-        //   text: 'You sign up for:',
-        //   icon: "success",
-        //   // text: `Please write this down so you don't forget!`,
-        // })
-        // .then((value) => {
-        //   if (value === true) {
-        //     this.props.history.push(`/volunteer-walk-up`)
-        //   }
-        // })
-      //   this.props.dispatch({
-      //     type: 'POST_WALKUP_INFO',
-      //     payload: this.props.match.params
-      // });
-      }
-
-      cancelButton = () => {
-        console.log('cancel btn click')
-        this.props.history.push(`/volunteer-walk-up/${this.props.match.params.id}`)
-    }
-
-    render() {
+      render() {
+        let infoDetails = this.props.reduxStore.SelectedShiftsReducer.map((details, id) => {
+            if( id == 0){
+        info = <p key={details.id}>{details.id}</p>
+            }
+            })
         return (
             <div className="WalkUpConfirm">
-                Verify your info
+                Confirm
                 <br />
-        {/* <h1>{this.props.badgeNumber}</h1> */}
-        {/* <h2>{this.props.match.params.id}</h2> */}
+                {info}
+                {infoDetails}
 
-        <form>
-        <p>Badge #{this.props.match.params.id}</p>
-            <p>First Name
-            <br />
-              <input
-              type="text"
-              placeholder="First name"
-              value={this.state.volunteerFirstName}
-              onChange={this.handleInputChange('volunteerFirstName')}>
-            </input> 
-            </p>
-            <p>Discord Name
-              <br/>
-              <input
-              type="text"
-              placeholder="Discord name"
-              value={this.state.discordName}
-              onChange={this.handleInputChange('discordName')}>
-            </input>
-            </p>
-            <p>Phone Number
-            <br/>     
-             <input
-              type="number"
-              placeholder="Phone number"
-              value={this.state.phoneNumber}
-              onChange={this.handleInputChange('phoneNumber')}>
-            </input>
-            </p>
-            <br/>
-            <br />
-        </form>
-            <button onClick={this.cancelButton}>Cancel</button>
-            <button onClick={this.verifyInfo}>Confirm</button>
+        {JSON.stringify(this.props.reduxStore.SelectedShiftsReducer)}
+        
+            <button onClick={this.confirm}>Confirm</button>
             </div>
         )
     }
