405c78d90a48579edff39ef6021e19dd007996e7
Nathan Wold <natew79@gmail.com>
2019-11-17 14:40:54 -0600

    walk up epic functional for new volunteers only

--- src/components/VolunteerWalkUp/WalkUpConfirm.js
+++ src/components/VolunteerWalkUp/WalkUpConfirm.js
@@ -8,45 +8,71 @@ import swal from 'sweetalert';
 
 class WalkUpConfirm extends Component {
 
-    confirm = () => {
-        swal({
-            title: `Thank You`,
-            icon: "success"
-          }).then(() => {
-             {
-                this.props.history.push(`/volunteer-walk-up/`)
-            }
-        })
+    componentDidMount() {
+        this.loadSelectedShifts();
     }
 
-      render() {
+    selectedShifts = []
+
+    loadSelectedShifts = () => {
+        this.props.dispatch({
+            type: 'FETCH_WALKUP_SHIFTS',
+            payload: this.props.match.params
+        });
+        this.props.reduxStore.SelectedShiftsReducer.map((selected) => (
+            this.props.reduxStore.VolunteerWalkUpReducer.map((shift) => {
+                if (selected === shift.ShiftID) {
+                    this.selectedShifts.push(shift)
+                }
+            })
+        ))
+    }
+
+
+    confirm = () => {
+        console.log(this.selectedShifts);
+        axios.put(`/api/walkup/selected/${this.props.match.params.id}`, this.selectedShifts)
+            .then(response => {
+                console.log(response)
+                // swal({
+                //     title: `Thank You`,
+                //     icon: "success"
+                // }).then(() => {
+                //     this.props.history.push(`/volunteer-walk-up/`)
+                // })
+            }).catch(error => {
+                console.log(error)
+            })
+    }
+
+    render() {
 
         return (
             <div className="WalkUpConfirm">
-                {/* Confirm */}
-                <br />
                 <h1>Thank You</h1>
-                <h2>You sign up for: </h2>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.reduxStore.SelectedShiftsReducer.map((info) =>(
-                    <tr>
-                        <td>{info}</td>
-                    </tr>
-                    ))}
-                </tbody>
-            </table>
-            <h4>Please write this down so you won't forget!</h4>
-
-{/* {JSON.stringify(this.props.reduxStore.SelectedShiftsReducer)} */}
-
-            <button onClick={this.confirm}>OKAY</button>
+                <h2>You signed up for: </h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Day</th>
+                            <th>Time</th>
+                            <th>Department</th>
+                            <th>Role</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.selectedShifts.map((shift) => (
+                            <tr key={shift.ShiftID}>
+                                <td>{moment(shift.ShiftDate).format('dddd')}</td>
+                                <td>{(shift.ShiftTime).slice(0, -3)}</td>
+                                <td>{shift.DepartmentName}</td>
+                                <td>{shift.RoleName}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <h4>Please write this down so you won't forget!</h4>
+                <button onClick={this.confirm}>OKAY</button>
             </div>
         )
     }
