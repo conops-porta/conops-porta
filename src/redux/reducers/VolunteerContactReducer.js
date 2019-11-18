81a0c5f0a30bff688847101503a02fb08cc7efab
Lilith Bentley <sbentley@gmail.com>
2019-11-18 14:56:16 -0600

    Add volunteer contact saga

--- src/redux/reducers/VolunteerContactReducer.js
+++ src/redux/reducers/VolunteerContactReducer.js
@@ -1,6 +1,6 @@
 const VolunteerContactReducer = (state = [], action) => {
   switch (action.type) {
-      case 'SET_VOLUNTEER_CONTACT':
+      case 'SET_VOLUNTEER_CONTACTS':
           return action.payload
       default:
           return state;
