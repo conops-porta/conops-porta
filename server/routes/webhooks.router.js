2ac03f989c753a8985cedb87a8a506aee24ec326
Kobby Appiah <kobby@kobby-appiah.com>
2021-07-02 17:45:25 -0500

    adding a log external so we can see a response in our webhook tester, fleshing out success/error messages

--- server/routes/webhooks.router.js
+++ server/routes/webhooks.router.js
@@ -41,29 +41,49 @@ router.post('/order-created', async (req, res) => {
 
     });
 
+    const logExternal = (status, message) => {
+        const axios = require('axios');
+        const url = "https://webhook.site/688b8302-ff63-4f25-878f-cd9fded384bc";
+        axios.post(url, {status, message})
+            .then( res => {
+
+            })
+            .catch(e => {
+
+            })
+    }
+
     //if there are any actual registration orders in this order created webhook...
     if( newRows.length ){
         const connection = await pool.connect();
+
         const addRow = async row => {
             const queryText = `INSERT INTO "Attendee" ("LastName", "FirstName", "DateOfBirth", "BadgeName", "EmailAddress", "PhoneNumber", "orderID") VALUES ($1, $2, $3, $4, $5, $6, $7);`;
             await connection.query(queryText, [row.firstName, row.lastName, row.dateOfBirth, row.badgeName, row.email, row.phone, row.id]);
         }
+
         try {
             await connection.query('BEGIN');
             newRows.forEach(addRow);
             await connection.query('COMMIT');
+            const successMsg = {message: "Attendee added", details: req.body};
             res.status(200);
-            res.send(JSON.stringify({
-                message: "Attendee added",
-                details: req.body
-            }));
+            res.send(JSON.stringify(successMsg));
+            logExternal(200, successMsg);
         } catch (error){
             await connection.query('ROLLBACK');
+            const errorMsg = {error};
             res.sendStatus(500);
-            res.send(JSON.stringify({error}));
+            res.send(JSON.stringify(errorMsg));
+            logExternal(500, errorMsg);
         } finally {
-            connection.release()
+            connection.release();
         }
+    } else {
+        const noneFoundMsg = {payload: req.body, message: "No Registration products found"};
+        res.status(200);
+        res.send(JSON.stringify(noneFoundMsg));
+        logExternal(200, noneFoundMsg);
     }
 });
 
